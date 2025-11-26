165d18a59ef12fd135d67132aec90b646abb06ff
mateovy <mateo.valenciay@outlook.com>
2025-11-25 20:59:47 -0500

    fix supabaseclient code

--- src/supabaseClient.mjs
+++ src/supabaseClient.mjs
@@ -1,6 +1,6 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = 'https://jtdmckpffriblcnfhkhi.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp0ZG1ja3BmZnJpYmxjbmZoa2hpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE2ODEwMzEsImV4cCI6MjA3NzI1NzAzMX0.aW37GPKj44YPWo476jBFgADdBxAqY_4luMYsBVdWWGE'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
